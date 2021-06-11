38334e9412469b53787cf7525f03b14a5c49726f
alex <alexizana10@gmail.com>
2021-06-11 15:43:37 +0400

    added form and input components

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,3 +1,5 @@
+import MealItemForm from "./MealItemForm";
+
 import classes from "./MealItem.module.css";
 
 const MealItem = (props) => {
@@ -10,7 +12,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <form></form>
+        <MealItemForm />
       </div>
     </li>
   );
