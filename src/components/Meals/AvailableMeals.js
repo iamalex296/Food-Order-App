0128102dc70e918958e3da5cbab1dd30ef6217e6
alex <alexizana10@gmail.com>
2021-06-11 15:02:05 +0400

    added MealItems component

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,6 @@
+import Card from "../UI/Cart";
+import MealItem from "./MealItem/MealItem";
+
 import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
@@ -29,12 +32,19 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
-    <li key={meal.id}>{meal.name}</li>
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
   ));
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
---
+++ src/components/UI/Cart.js
@@ -0,0 +1,7 @@
+import classes from "./Card.module.css";
+
+const Cart = (props) => {
+  return <div className={classes.card}>{props.children}</div>;
+};
+
+export default Cart;
